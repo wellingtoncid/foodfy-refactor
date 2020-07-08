d6c287abf110de8d692180851f72a19c563cf555
Wellington Cid <wellington.cid@hotmail.com>
2020-07-08 17:43:00 -0300

    up put and delete

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -32,7 +32,7 @@ exports.show = function (req, res) {
 
 }
 
-exports.edit = function (req,res) {
+exports.edit = function (req, res) {
 
     const { id } = req.params
 
@@ -77,3 +77,48 @@ exports.post = function (req, res) {
     // return res.send(req.body)
 }
 
+exports.put = function (req, res) {
+    const { id } = req.body
+    let index = 0
+
+    const foundRecipe = data.recipes.find(function (recipe, foundIndex) {
+        if (id == recipe.id) {
+            index = foundIndex
+            return true
+        }
+    })
+
+    if (!foundRecipe) return res.send('Recipe not found')
+
+    const recipe = {
+        ...foundRecipe,
+        ...req.body,
+        id: Number(foundRecipe.id)
+    }
+
+    data.recipes[index] = recipe
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
+        if (err) return res.send("Write file error")
+
+        return res.redirect(`/admin/recipes/${id}`)
+    })
+
+}
+
+exports.delete = function (req, res) {
+    const { id } = req.body
+
+    const filteredRecipes = data.recipes.filter(function (recipe) {
+        return recipe.id != id
+    })
+
+    data.recipes = filteredRecipes
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
+        if (err) return res.send("Write file error")
+
+        return res.redirect('/admin/recipes')
+    })
+
+}
--- routes.js
+++ routes.js
@@ -19,8 +19,8 @@ routes.get("/admin/recipes/create", recipes.create); // Mostrar formulário de n
 routes.get("/admin/recipes/:id", recipes.show); // Exibir detalhes de uma receita
 routes.get("/admin/recipes/:id/edit", recipes.edit); // Mostrar formulário de edição de receita
 routes.post("/admin/recipes", recipes.post); // Cadastrar nova receita
-// routes.put("/admin/recipes", recipes.put); // Editar uma receita
-// routes.delete("/admin/recipes", recipes.delete); // Deletar uma receita
+routes.put("/admin/recipes", recipes.put); // Editar uma receita
+routes.delete("/admin/recipes", recipes.delete); // Deletar uma receita
 
 
 module.exports = routes
