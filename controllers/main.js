9d73a558b4fed6bbb12f050189cb2e5089ba8b97
Wellington Cid <wellington.cid@hotmail.com>
2020-07-09 14:50:43 -0300

    update routes = user/admin

--- controllers/main.js
+++ controllers/main.js
@@ -1,7 +1,7 @@
-const data = require("../data.js")
+const data = require('../data.json')
 
 exports.index = function (req, res) {
-    return res.render('home', { items: data })
+    return res.render('home', { recipes: data.recipes })
 }
 
 exports.about = function (req, res) {
@@ -9,18 +9,18 @@ exports.about = function (req, res) {
 }
 
 exports.recipes = function (req, res) {
-    return res.render('recipes', { items: data })
+    return res.render('recipes', { recipes: data.recipes })
 }
 
 exports.recipeIndex = function (req, res) {
-    const recipe = data  // Array de receitas carregadas do data.js
+    const recipe = data     // Array de receitas carregadas do data.js
     const recipeIndex = req.params.index
 
     if (!recipe) {
         return res.render('notfound')
     }
 
-    return res.render('recipe', { item: data[recipeIndex] })
+    return res.render('recipe', { recipes: data.recipes[recipeIndex] })
 }
 
 // server.use(function (req, res) {
